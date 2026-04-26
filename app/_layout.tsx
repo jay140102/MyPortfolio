458b9cf5551ed14dacabf255fc8463985d0d98a2
jay140102 <jpratap581@gmail.com>
2026-04-27 05:02:23 +0530

    Add Google site verification meta tag

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,14 @@
 import { Stack } from "expo-router";
+import Head from "expo-router/head";
+
 
 export default function RootLayout() {
   return (
-    <Stack
+    <>
+      <Head>
+        <meta name="google-site-verification" content="xgzSNrOnjSCbbRWyLw8K3qaQUwCSN2ZUnEXqwP1qej8" />
+      </Head>
+      <Stack
       screenOptions={{
         headerShown: false,
         contentStyle: { backgroundColor: '#FFFFFF' },
@@ -26,6 +32,7 @@ export default function RootLayout() {
           title: 'Privacy Policy | QuickChat WA'
         }}
       />
-    </Stack>
+      </Stack>
+    </>
   );
 }
